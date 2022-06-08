f8741f093c58f67ca81e17ae34775fb4838fcc09
Harinder Labana <sunny.labana@gmail.com>
2022-06-07 20:05:09 -0400

    created test cases for assertEqual.js and head.js and created modules for each to export

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,4 @@
-// // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -7,6 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,16 +1,6 @@
-// assertEqual function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // head function, return index 0
-const head = function(actual) {
+const head = function (actual) {
   return actual[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
